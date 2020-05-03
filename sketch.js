9658d6968adbe963e207613194980d0b0d5efdd0
KrishKabob <63270377+KrishKabob@users.noreply.github.com>
2020-05-03 12:11:21 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,18 +1,38 @@
-var leg, leg2, body, head
+var body,head,leg,leg2;
+var dog;
 
 function setup() {
   createCanvas(400, 400);
-
-  var leg = createSprite(100, 300, 20, 50);
-  leg.velocityY = 0;
-  var leg2 = createSprite(270, 300, 20, 50);
-  leg2.velocityY = 0;
-  var body = createSprite(200, 285, 200, 20);
-  var head = createSprite(275, 265, 40, 40);
+  body = createSprite(210, 285, 140, 40);
+  body.shapeColor = "black";
+  
+  head = createSprite(275, 265, 40,40);
+  head.shapeColor = "black";
+  
+  leg = createSprite(150, 300, 20, 50);
+  leg.shapeColor = "black";
+  
+  leg2 = createSprite(270, 300, 20, 50);
+  leg2.shapeColor = "black";
+  
+  dog = new Group();
+  dog.add(leg);
+  dog.add(leg2);
+  dog.add(body);
+  dog.add(head);
 }
 
 function draw() {
-  background("white");
-
-drawSprites();
+  background(255);
+  
+  if(keyDown("left")){
+    dog.setVelocityXEach(-2);
+  }
+  
+  if(keyDown("right")){
+    dog.setVelocityXEach(2);
+  }
+  
+  drawSprites();
 }
+
